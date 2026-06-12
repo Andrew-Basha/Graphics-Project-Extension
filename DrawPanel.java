f94ab6fcc4828a1dbc22c4ab8dd32844ff27e012
Andrew-Basha <Andrewb298@nycstudents.net>
2026-06-12 08:04:32 -0400

    Add files via upload

--- DrawPanel.java
+++ DrawPanel.java
@@ -119,7 +119,7 @@ class DrawPanel extends JPanel implements MouseListener {
             }
         }
         for (int i = 0; i < hand.getHand().size(); i++) {
-            if ((hand.getHand().get(i).getSuit().equals("joker") || currentCard.getSuit().equals("joker")) && playerHand.getHand().size() > 2) {
+            if ((hand.getHand().get(i).getSuit().equals("joker") && playerHand.getHand().size() > 2 && Math.random() >= .25) || currentCard.getSuit().equals("joker")) {
                 return i;
             }
         }
